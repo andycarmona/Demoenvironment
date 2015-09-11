ed3779f9b0476c1000494c360bb789f9f1e507c8
andycarmona <andres.carmona@cybercom.com>
2015-09-11 15:08:17 +0200

    Bugfix

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,10 +1,9 @@
 'use strict';
 define([
 	'angular',
-	'angularRoute',
-    'angularBootstrap'
+	'angularRoute'
 ], function(angular) {
-	angular.module('myApp.view1', ['ngRoute','ui.bootstrap','firebase'])
+	angular.module('myApp.view1', ['ngRoute','firebase'])
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.when('/view1', {
 			templateUrl: 'view1/view1.html',
@@ -12,17 +11,13 @@ define([
 		});
 	}])
 	.controller('View1Ctrl', ['$firebaseObject','$firebaseArray','$scope',function($firebaseObject,$firebaseArray,$scope) {
-		var ref = new Firebase('https://blazing-torch-4150.firebaseio.com');
+				var ref = new Firebase('https://blazing-torch-4150.firebaseio.com');
         
         $scope.messages=$firebaseArray(ref);
         
-        $scope.addMessage = function(){
-            $scope.messages.$add({
-                text:$scope.newMessageText
-        });
-        //var syncObject =$firebaseObject(ref);
-        
-        //syncObject.$bindTo($scope,"data");
+        $scope.addMessage= function(){
+        $scope.messages.$add({text:$scope.newMessageText})
+        }
         
 	}]);
 });
